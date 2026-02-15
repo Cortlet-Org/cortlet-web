8d04d65fe02e1f61e04734207b90460043bc2f1c
Cortlet-Software <support@cortlet.com>
2026-02-15 20:08:02 +0530

    More csp urls added

--- next.config.ts
+++ next.config.ts
@@ -11,31 +11,51 @@ const nextConfig = {
                         key: "Content-Security-Policy",
                         value: `
 default-src 'self';
+
 img-src 'self' https://cdn.cortlet.com https://developers.google.com https://github.githubassets.com data:;
-script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://www.googleapis.com https:;
+
+script-src 
+  'self' 
+  'unsafe-inline'
+  'unsafe-eval'
+  https://www.gstatic.com
+  https://apis.google.com
+  https://www.googleapis.com
+  https://accounts.google.com;
+
 style-src 'self' 'unsafe-inline' https:;
-connect-src 'self' blob: data:
+
+connect-src 
+  'self'
+  blob:
+  data:
   https://api.openrouter.ai
   https://api.resend.com
   https://identitytoolkit.googleapis.com
   https://securetoken.googleapis.com
   https://firebasestorage.googleapis.com
   https://firestore.googleapis.com
-  https://www.googleapis.com
   https://firebase.googleapis.com
-  https://cortlet-web.firebaseapp.com
-  https://cortlet-web.web.app
+  https://www.googleapis.com
+  https://apis.google.com
+  https://github.com
+  https://api.github.com
   https://cdn.cortlet.com;
+
 font-src 'self' https://cdn.cortlet.com data:;
-frame-src 
+
+frame-src
   'self'
   https://accounts.google.com
   https://*.google.com
   https://*.googleusercontent.com
+  https://apis.google.com
+  https://github.com
   https://cortlet-web.firebaseapp.com
   https://cortlet-web.web.app
   https://*.firebaseapp.com;
-frame-ancestors 'self' https://accounts.google.com;
+
+frame-ancestors 'self';
 `.replace(/\s+/g, " "),
                     },
                 ],
